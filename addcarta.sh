15545a105a2ae97c0680c4673e38659af99a7ef6
Felipe Góes <ogoes.felipe@gmail.com>
2021-09-01 22:30:23 -0300

    <WIP> Terminei a lógica de incluir Tipo.   - Atualizei as variáveis locais   - Apliquei uma validação para as outras memórias

--- addcarta.sh
+++ addcarta.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Fecha se algum comando falhar
+set -e
 
 # Available colors
 # Black        0;30     Dark Gray     1;30
@@ -14,18 +16,38 @@ function addcarta(){
   # Opções de cores. NC serve para fechar bloco de cor.
   local PINK='\033[0;35m'
   local NC='\033[0m'
-  
+
+  local nomeCarta
+  local qtdCarta
+  local descricaoCarta
+  local raridadeCarta
+  local tipoCarta
+  local subtipoCarta
+  local combateCarta
+  local custoCarta
+  local habilidadeCarta
+
   # Mostra todas os campos de uma carta.
   echo "Carta:"
   echo "Nome | Quantidade | Descrição | Raridade | Tipo | Subtipo | Combate | Custo | Habilidade"
   echo ""
 
   # Lê campos básicos. Os dados não precisam ser tratados.
-  read -p "Nome: " local nomeCarta
-  read -p "Quantidade: " local qtdCarta
-  read -p "Descrição: " local descricaoCarta
+  while [[ "$nomeCarta" == "" ]]
+  do
+    read -p "Nome: " nomeCarta
+    nomeCarta=$( echo "$nomeCarta" | sed "s/^ *//g" ) # Remove trailing spaces
+  done
+  while [[ ! $qtdCarta =~ (^[0-9]$)|(^[0-9][0-9]$) ]]
+  do
+    read -p "Quantidade: " qtdCarta
+    qtdCarta=$( echo "$qtdCarta" | sed "s/^ *//g" ) # Remove trailing spaces
+  done
+  read -p "Descrição: " descricaoCarta
+  descricaoCarta=$( echo "$descricaoCarta" | sed "s/^ *//g" ) # Remove trailing spaces
 
-  # Obtém do banco de dados os nomes válidos de raridade e converte em um array.
+  # Seção Raridade
+  ## Obtém do banco de dados os nomes válidos de raridade e converte em um array.
   local raridades=( 
     $(
       psql -U postgres -d mtg -c "select nome as Raridade from raridade;" |
@@ -35,45 +57,72 @@ function addcarta(){
   raridades[-2]="${raridades[-2]} ${raridades[-1]}"
   unset raridades[-1]
 
-  # Exibe para o usuário todos os nomes válidos de raridade numa cor de destaque.
-  coloredOut=$(
+  ## Exibe para o usuário todos os nomes válidos de raridade numa cor de destaque.
+  local textoColorido=$(
   echo -e "Raridades: ${raridades[@]}" |
     awk '{print $1 " " $2 ", " $3 ", " $4 ", " $5 " " $6}'
   )
-  echo -e "${PINK}$coloredOut${NC}"
+  echo -e "${PINK}$textoColorido${NC}"
 
-  # Lê o input do usuário permitindo apenas uma entrada válida.
+  ## Lê o input do usuário permitindo apenas uma entrada válida.
   while [[ ! " ${raridades[*]} " =~ " ${raridadeCarta} " ]]
   do
     read -p "Raridade: " raridadeCarta
     raridadeCarta=$( echo "$raridadeCarta" | sed "s/^ *//g" ) # Remove trailing spaces
   done
 
+  # Seção Tipo
+  # Obtém do banco de dados os nomes válidos de tipo e converte em um array.
   local tipos=( 
     $(
       psql -U postgres -d mtg -c "select nome as Tipo from tipo;" |
         sed -n "3,10p" | xargs
     )
   )
-  coloredOut=$(
+  textoColorido=$(
   echo -e "Tipos: ${tipos[@]}" |
     awk '{print $1 " " $2 ", " $3 ", " $4 ", " $5 ", " $6 " " $7 ", " $8 ", " $9 " " $10 ", " $11}'
   )
-  echo -e "${PINK}$coloredOut${NC}"
+  echo -e "${PINK}$textoColorido${NC}"
   local tiposCarta=()
-  local tipoCarta="random"
 
-  while [[ "$tipoCarta" != "" ]]
+  local adicionaMais="Não"
+  while [[ ! " ${tipos[*]} " =~ " ${tipoCarta} " ]]
   do
-    read -p "Tipo: " local tipoCarta
-    tiposCarta+=( "$tipoCarta" )
+    read -p "Tipo: " tipoCarta
+    tipoCarta=$( echo "$tipoCarta" | sed "s/^ *//g" ) # Remove trailing spaces
+    if [[ " ${tipos[*]} " =~ " ${tipoCarta} " ]]
+    then
+      if [[ ! " ${tiposCarta[*]} " =~ " ${tipoCarta} " ]]
+      then
+        tiposCarta+=( "$tipoCarta" )
+        read -p "Você deseja adicionar mais algum tipo?(Sim-Não): " adicionaMais
+        if [[ ${adicionaMais^^} == *"S"* ]]
+        then
+          tipoCarta=""
+        fi
+      else
+       echo "Você já selecionou este campo. O script vai seguir para o próximo."
+      fi
+    fi
   done
+  # while [[ "${#tiposCarta[@]}" == 0 ]]
+  # do
+  #   while [[ "$tipoCarta" != "" ]]
+  #   do
+  #     read -p "Tipo: " tipoCarta
+  #     if [[ "$tipoCarta" != "" ]]
+  #     then
+  #       tiposCarta+=( "$tipoCarta" )
+  #     fi
+  #   done
+  # done
   # if [[ "$tipoCarta" == "Criatura" ]]
 
-  read -p "Subtipo: " local subtipoCarta
-  read -p "Combate: " local combateCarta
-  read -p "Custo: " local custoCarta
-  read -p "Habilidade: " local habilidadeCarta
+  read -p "Subtipo: " subtipoCarta
+  read -p "Combate: " combateCarta
+  read -p "Custo: " custoCarta
+  read -p "Habilidade: " habilidadeCarta
   # psql -U postgres -d mtg -c "select * from carta;"
 }
 
